9d4f388179877095b2f27b0f6d8ce56dfd3c590d
babyfuchia <baby.fuchia@gmail.com>
2022-12-16 02:25:30 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-fe661bb708dcee67.js
+++ _next/static/chunks/webpack-fe661bb708dcee67.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "63d28cf55280cb81" + ".css";
+/******/ 			return "static/css/" + "e04215f4899c9de5" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
